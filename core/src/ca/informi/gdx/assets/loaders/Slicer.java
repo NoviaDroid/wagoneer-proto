7e416801d91b7abc4dd835b93d0dc4e741e7149f
Justin Bowes <jbowes@informi.ca>
2015-01-23 15:51:22 -0500

    whoops

--- core/src/ca/informi/gdx/assets/loaders/Slicer.java
+++ core/src/ca/informi/gdx/assets/loaders/Slicer.java
@@ -5,7 +5,29 @@ import ca.informi.gdx.graphics.g2d.ProceduralTextureAtlas;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 
-public interface Slicer {
+public abstract class Slicer {
 
-	void slice(ProceduralTextureAtlas atlas, Texture texture, Pixmap pixmap);
+	private boolean topDown;
+
+	protected final ProceduralTextureAtlas atlas;
+	protected final Pixmap pixmap;
+	protected final Texture texture;
+
+	public Slicer(final ProceduralTextureAtlas atlas, final Texture texture, final Pixmap pixmap, final boolean topDown) {
+		super();
+		this.atlas = atlas;
+		this.texture = texture;
+		this.pixmap = pixmap;
+		this.topDown = topDown;
+	}
+
+	public abstract void slice();
+
+	protected void setTopDown(final boolean topDown) {
+		this.topDown = topDown;
+	}
+
+	protected void slice(final int x, final int y, final int w, final int h) {
+
+	}
 }
